8005351a109723c02101d7ebb30ac726fd2241af
Insonic <brunodealmeida17@yahoo.com>
2022-07-16 15:39:22 -0300

    atualizando post api

--- apirestfile/views.py
+++ apirestfile/views.py
@@ -1,7 +1,7 @@
 from telnetlib import STATUS
 from django.shortcuts import render
-from rest_framework import generics
-import io, csv, pandas as pd
+from rest_framework import generics, status
+import pandas as pd
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Fileupload
@@ -10,19 +10,18 @@ from apirestfile.serializers import FileUploadSerializer, ReadFileView
 
 
 
-class UploadFileCSV(generics.CreateAPIView):
-    queryset = Fileupload.objects.all()
-    serializer_class = FileUploadSerializer
-    
-    
-    def post(self, request, *args, **kwargs):
-        
+class UploadFileCSV(generics.CreateAPIView):   
+    serializer_class = FileUploadSerializer  
+
+    def get(self, request):
+       return Response('Classe para upload de arquivos')
+
+    def post(self, request, *args, **kwargs):        
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         file = serializer.validated_data['file'] 
         xid = serializer.validated_data['xid']       
-        reader = pd.read_csv(file)
-        monitoramento_list = []
+        reader = pd.read_csv(file)        
         for _, row in reader.iterrows():
             new_file = Fileupload(
                        xid=xid,
@@ -30,10 +29,10 @@ class UploadFileCSV(generics.CreateAPIView):
                        dado= row['dado'],                       
                        )
                        
-            new_file.save()
-        Fileupload.objects.bulk_create(monitoramento_list)
-
-        return Response("upload Feito")
+            new_file.save()    
+        return Response({"status": "success"},
+                        status.HTTP_201_CREATED)
+                            
 
 
 class ReadFileView(viewsets.ModelViewSet):
